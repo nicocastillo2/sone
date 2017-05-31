ae196e99889ceee543ccb14dfa73666e64a44c6c
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-05-31 13:37:35 -0500

    Add AWS region to carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   require 'carrierwave/storage/fog'
-  
+
   CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
@@ -8,6 +8,7 @@ if Rails.env.production?
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION'],
     }
     config.fog_directory = ENV['AWS_BUCKET']
     config.fog_public = true
