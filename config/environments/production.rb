cf783f6d6384bd10382f81638965a610c3fc31a4
RobertoCarrilloAvila <roberto_ironmaiden@live.com.mx>
2017-06-07 11:20:29 -0500

    add default_url_options for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => "sonedev.herokuapp.com" }
   #sparkpost configuration
   config.action_mailer.delivery_method = :sparkpost
 end
