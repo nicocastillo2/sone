9794a4c37e537700bc36f0ca03f41da3a2d758b9
Roberto Carrillo <roberto_ironmaiden@live.com.mx>
2017-07-07 14:06:33 -0500

    login temporal view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,22 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  layout :layout_by_resource
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      false
+    else
+      "application"
+    end
   end
 
 end
