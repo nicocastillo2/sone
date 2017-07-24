8d4e3503809fb1e562bf06822fba6f87354fcdd0
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-07-24 16:25:01 -0500

    Add dynamic topics to contacts and tmp_topics to campaign

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705171900) do
+ActiveRecord::Schema.define(version: 20170721190807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170705171900) do
     t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "tmp_topics", default: [], array: true
     t.index ["user_id"], name: "index_campaigns_on_user_id"
   end
 
@@ -44,6 +45,7 @@ ActiveRecord::Schema.define(version: 20170705171900) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "topics", default: "{}", null: false
     t.index ["campaign_id"], name: "index_contacts_on_campaign_id"
   end
 
