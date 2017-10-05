65f1f1e692071a8cd58c10d67eddb4dd0347ad87
Mauricio García <m4ur1c1o.gg@gmail.com>
2017-10-05 18:01:04 -0500

    Modify table with sent contacts to only show unanswered and active contacts

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -15,7 +15,8 @@ class CampaignsController < ApplicationController
   def show
     date_range = Campaign.receive_date('1')
     @nps = Nps.for_campaign(@campaign.id, date_range[0], date_range[1], @campaign.tmp_topics)
-    @contacts_sent = Campaign.includes(contacts: [:answer]).find(params[:id]).contacts.where(valid_info: true, status: '1').paginate(:page => params[:page])
+    # @contacts_sent = Campaign.includes(contacts: [:answer]).find(params[:id]).contacts.where(valid_info: true, status: '1').paginate(:page => params[:page])
+    @contacts_sent_pendants_actives = Campaign.includes(contacts: [:answer]).find(params[:id]).contacts.where(valid_info: true, status: '1').where("sent_date > ?", DateTime.now - 48.hours).paginate(:page => params[:page])
     @contacts_not_sent = Campaign.includes(contacts: [:answer]).find(params[:id]).contacts.where(valid_info: true, status: ['0', '3']).paginate(:page => params[:page])
     @topics = Campaign.find(params[:id]).tmp_topics
     @campaign.update({new_answers: 0})
