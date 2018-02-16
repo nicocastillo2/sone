57e0fdedf097f9f12a18411546eb54f0cf09bd63
Nico <nicocastillo@hotmail.com>
2018-02-16 17:45:57 +0100

    meta

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,15 +4,26 @@ class StaticController < ApplicationController
   # http_basic_authenticate_with name: "kheper", password: "1234kh"
 
   def homepage
+    @title="SONE - Software de Experiencia de Cliente" 
+    @description="Evalúa la experiencia de tus clientes y encuentra áreas de oportunidad para tu negocio"   
     # redirect_to campaigns_path if user_signed_in?
   end
 
   def pricing
+    @title="SONE - Planes y Precios" 
+    @description="Planes que se ajustan a tus necesidades"
+
   end
 
   def terms
+    @title="SONE - Términos y Condiciones" 
+    @description="Conoce nuestros términos y condiciones para la utilización del servicio"
+
   end
 
   def politics
+    @title="SONE - Política de Privacidad" 
+    @description="Acerca de nuestra política de privacidad"
+
   end
 end
