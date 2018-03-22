58bda3046c1d09a28a259ee2a0c415a695ce6317
jjccc <jjcanocortes@outlook.com>
2018-03-22 18:40:26 +0100

    Test deploy

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,6 +7,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
+    # It must be stored in this directory for avoiding reset after each deploy
     "public/system/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
