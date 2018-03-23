0e2e1c4b055865ff7e5884b68d146534531f48b3
Nico <nicocastillo@hotmail.com>
2018-03-23 18:18:09 +0100

    register

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,11 +4,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     CampaignMailer.welcome(current_user).deliver_now
     current_user.update(terms: true)
-    campaigns_path # Or :prefix_to_your_route
+    new_campaign_path # Or :prefix_to_your_route
 
   end
 
   def after_update_path_for(resource)
-    campaigns_path
+    new_campaign_path
   end
 end
