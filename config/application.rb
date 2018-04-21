761a110432f136299f2f4cf9527a6d5a1888504e
jjccc <jjcanocortes@outlook.com>
2018-04-21 07:08:49 +0200

    Refactor password mailer image links

--- config/application.rb
+++ config/application.rb
@@ -11,15 +11,9 @@ Bundler.require(*Rails.groups)
 module Sone
   class Application < Rails::Application
     
-    # config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
-      # r301 %r{^\/blog(\/?.*)$}, 'http://159.89.86.242/blog/$1'
-      # # r301 %r{/blog/(\w+\/?)}, 'http://159.89.86.242/blog/$1'
-    # end
-    
     config.middleware.insert(0, Rack::ReverseProxy) do
       reverse_proxy_options preserve_host: true
       reverse_proxy /^\/blog(\/?.*)$/, 'https://blog.sone-app.com/blog/$1'
-      # reverse_proxy /^\/blog(\/?.*)$/, 'http://159.89.86.242/blog/$1'
     end
     
     # Initialize configuration defaults for originally generated Rails version.
